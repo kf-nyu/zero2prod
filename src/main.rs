fa4e57097f17d2bba4347d77918756490e619358
2/dev/null <kf2623@nyu.edu>
2026-08-10 14:25:05 -0400

    Project skeleton and foundation

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+// async fn health_check(req: HttpRequest) -> impl Responder {
+async fn health_check() -> impl Responder {
+    //let name = req.match_info().get("name").unwrap_or("world");
+
+    // format!("Hello {}!", &name)
+    //HttpResponse::Ok().finish()
+    HttpResponse::Ok()
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(),std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+//            .route("/", web::get().to(greet))
+//            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
