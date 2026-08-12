1c6785fd17463fe11c7a6250e4b2788b05cf6585
2/dev/null <kf2623@nyu.edu>
2026-08-12 13:35:44 -0400

    Add PostgreSQL setup and subscriptions migration

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
 //! src/lib.rs
 
-use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
+use actix_web::{App, HttpResponse, HttpServer, web};
 use std::net::TcpListener;
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 // async fn health_check(req: HttpRequest) -> impl Responder {
 async fn health_check() -> HttpResponse {
     //let name = req.match_info().get("name").unwrap_or("world");
@@ -12,19 +19,25 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 // #[tokio::main]
 // Notice the different singnature!
 // We return 'Server' on the happy path and we dropped the 'async' keyword
 // We have no .await cal, so itis not needed anymore
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
-            App::new()
-//            .route("/", web::get().to(greet))
-//            .route("/{name}", web::get().to(greet))
-                .route("/health_check", web::get().to(health_check))
-        })
-        .listen(listener)?
-        .run();
+        App::new()
+            //            .route("/", web::get().to(greet))
+            //            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+            // A new entry in our routing table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     //No .await here!
     Ok(server)
 }
